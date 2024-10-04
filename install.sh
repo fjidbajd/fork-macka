36bc33e909b045def943c4205a3471ed3770e9ff
fjidbajd <128946062+fjidbajd@users.noreply.github.com>
2024-10-04 15:00:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9634,11 +9634,8 @@ menu() {
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
     checkWgetShowProgress
-    echoContent red "\n=========================== 推广区============================"
-    echoContent red "                                              "
-    echoContent green "VPS选购攻略：https://www.v2ray-agent.com/archives/1679975663984"
-    echoContent green "年付10美金低价VPS AS4837：https://www.v2ray-agent.com/archives/racknerdtao-can-zheng-li-nian-fu-10mei-yuan"
-    echoContent red "=============================================================="
+
+
     if [[ -n "${coreInstallType}" ]]; then
         echoContent yellow "1.重新安装"
     else
