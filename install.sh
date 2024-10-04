75298b036eaa4fc4cc682b1c2be41eefac8ad9fe
fjidbajd <128946062+fjidbajd@users.noreply.github.com>
2024-10-04 15:14:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9632,7 +9632,6 @@ menu() {
     echoContent green "当前版本：v3.3.18"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
-    
     showInstallStatus
     checkWgetShowProgress
     echoContent red "\n=============================================================="
